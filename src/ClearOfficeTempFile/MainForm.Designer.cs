4da47265a3f0ab9a5171e10a69fe817d7bbd00d6
lijianzhq <804390150@qq.com>
2017-11-13 22:05:51 +0800

    修改删除临时文件工具的窗口布局，适应窗口最大化

--- src/ClearOfficeTempFile/MainForm.Designer.cs
+++ src/ClearOfficeTempFile/MainForm.Designer.cs
@@ -34,13 +34,15 @@
             this.dataGrid = new System.Windows.Forms.DataGridView();
             this.File = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Location = new System.Drawing.Point(9, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 12);
             this.label1.TabIndex = 0;
@@ -48,7 +50,7 @@
             // 
             // txt_Path
             // 
-            this.txt_Path.Location = new System.Drawing.Point(95, 28);
+            this.txt_Path.Location = new System.Drawing.Point(92, 12);
             this.txt_Path.Name = "txt_Path";
             this.txt_Path.Size = new System.Drawing.Size(401, 21);
             this.txt_Path.TabIndex = 1;
@@ -56,7 +58,7 @@
             // 
             // btn_StartWork
             // 
-            this.btn_StartWork.Location = new System.Drawing.Point(516, 28);
+            this.btn_StartWork.Location = new System.Drawing.Point(513, 12);
             this.btn_StartWork.Name = "btn_StartWork";
             this.btn_StartWork.Size = new System.Drawing.Size(104, 23);
             this.btn_StartWork.TabIndex = 3;
@@ -72,11 +74,11 @@
             this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.File,
             this.Status});
-            this.dataGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dataGrid.Location = new System.Drawing.Point(0, 75);
+            this.dataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid.Location = new System.Drawing.Point(0, 43);
             this.dataGrid.Name = "dataGrid";
             this.dataGrid.RowTemplate.Height = 23;
-            this.dataGrid.Size = new System.Drawing.Size(632, 291);
+            this.dataGrid.Size = new System.Drawing.Size(632, 323);
             this.dataGrid.TabIndex = 4;
             // 
             // File
@@ -92,20 +94,30 @@
             this.Status.Name = "Status";
             this.Status.ReadOnly = true;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_Path);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_StartWork);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(632, 43);
+            this.panel1.TabIndex = 5;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 366);
             this.Controls.Add(this.dataGrid);
-            this.Controls.Add(this.btn_StartWork);
-            this.Controls.Add(this.txt_Path);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "MainForm";
             this.Text = "移除office~$这种临时文件";
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -117,6 +129,7 @@
         private System.Windows.Forms.DataGridView dataGrid;
         private System.Windows.Forms.DataGridViewTextBoxColumn File;
         private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
