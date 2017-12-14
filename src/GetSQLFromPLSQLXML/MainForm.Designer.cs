c46ab4bb772225928ba751efb06e192a2061da09
lijianzhq <804390150@qq.com>
2017-12-15 00:24:18 +0800

    新增一个处理sql语句
    新增一个处理sql语句，快速生成大量sql语句过程中会需要使用的小工具

---
+++ src/GetSQLFromPLSQLXML/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GetSQLFromPLSQLXML
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_inputXMLFilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_outputPath = new System.Windows.Forms.TextBox();
+            this.btn_startGet = new System.Windows.Forms.Button();
+            this.btn_browseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "xml文件路径：";
+            // 
+            // txt_inputXMLFilePath
+            // 
+            this.txt_inputXMLFilePath.Location = new System.Drawing.Point(100, 31);
+            this.txt_inputXMLFilePath.Name = "txt_inputXMLFilePath";
+            this.txt_inputXMLFilePath.Size = new System.Drawing.Size(331, 21);
+            this.txt_inputXMLFilePath.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "输出路径：";
+            // 
+            // txt_outputPath
+            // 
+            this.txt_outputPath.Location = new System.Drawing.Point(100, 85);
+            this.txt_outputPath.Name = "txt_outputPath";
+            this.txt_outputPath.Size = new System.Drawing.Size(331, 21);
+            this.txt_outputPath.TabIndex = 3;
+            // 
+            // btn_startGet
+            // 
+            this.btn_startGet.Location = new System.Drawing.Point(222, 142);
+            this.btn_startGet.Name = "btn_startGet";
+            this.btn_startGet.Size = new System.Drawing.Size(75, 23);
+            this.btn_startGet.TabIndex = 4;
+            this.btn_startGet.Text = "开始抽取";
+            this.btn_startGet.UseVisualStyleBackColor = true;
+            this.btn_startGet.Click += new System.EventHandler(this.btn_startGet_Click);
+            // 
+            // btn_browseFile
+            // 
+            this.btn_browseFile.Location = new System.Drawing.Point(437, 30);
+            this.btn_browseFile.Name = "btn_browseFile";
+            this.btn_browseFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_browseFile.TabIndex = 5;
+            this.btn_browseFile.Text = "浏览...";
+            this.btn_browseFile.UseVisualStyleBackColor = true;
+            this.btn_browseFile.Click += new System.EventHandler(this.btn_browseFile_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 188);
+            this.Controls.Add(this.btn_browseFile);
+            this.Controls.Add(this.btn_startGet);
+            this.Controls.Add(this.txt_outputPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_inputXMLFilePath);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "根据PLSQL导出的xml文件抽取sql语句";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_inputXMLFilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_outputPath;
+        private System.Windows.Forms.Button btn_startGet;
+        private System.Windows.Forms.Button btn_browseFile;
+    }
+}
+
---
+++ src/GetSQLFromPLSQLXML/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GetSQLFromPLSQLXML
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 窗体加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            txt_outputPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        }
+
+        /// <summary>
+        /// 浏览文件按钮单击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_browseFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 执行抽取sql语句
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_startGet_Click(object sender, EventArgs e)
+        {
+            Task<Result> t = new Task<Result>((o) =>
+            {
+                String[] input = o as String[];
+                return Do(input[0], input[1]);
+            }, new String[] { txt_inputXMLFilePath.Text, txt_outputPath.Text });
+            t.ContinueWith((res) =>
+            {
+                MessageBox.Show(String.Format("操作完成！操作结果：{0}，结果情况：{1}", res.Result.Status, res.Result.Msg));
+            });
+            t.Start();
+        }
+
+        /// <summary>
+        /// 执行任务的方法
+        /// </summary>
+        /// <param name="xmlFilePath"></param>
+        /// <param name="outputPath"></param>
+        private Result Do(String xmlFilePath, String outputPath)
+        {
+            Result res = new Result()
+            {
+                Status = false
+            };
+            if (String.IsNullOrEmpty(xmlFilePath))
+                return res;
+            try
+            {
+                String xmlFileName = Path.GetFileNameWithoutExtension(xmlFilePath);
+                String outputFilePath = Path.Combine(outputPath, xmlFileName + ".sql");
+                XDocument doc = XDocument.Load(xmlFilePath);
+                IEnumerable<String> sqlList = doc.Descendants("SQLSTR").Select(e => e.Value);
+                using (StreamWriter sw = new StreamWriter(File.Create(outputFilePath)))
+                {
+                    foreach (String item in sqlList)
+                        sw.WriteLine(item);
+                    sw.WriteLine("");
+                }
+                res.Status = true;
+            }
+            catch (Exception ex)
+            {
+                res.Status = false;
+                res.Msg = ex.Message;
+            }
+            return res;
+        }
+
+        public class Result
+        {
+            public Boolean Status;
+            public String Msg;
+        }
+    }
+}
