21bad9372483f728e3f11796fbc69f6eb17fa61e
jianl <804390150@qq.com>
2018-09-03 16:06:19 +0800

    新增一个读取.NET安装版本的小工具。

---
+++ src/ColorPicker.UI/MainForm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+
+//自定义命名空间
+using DreamCube.Foundation.Basic.Win32API.API;
+using DreamCube.Foundation.Basic.Win32API.Enums;
+using DreamCube.WinForm.Common;
+
+namespace ColorPicker.UI
+{
+    public partial class MainForm : Form
+    {
+        #region "私有字段"
+
+        private System.Threading.Timer timer = null;
+
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 处理窗体消息的方法
+        /// </summary>
+        /// <param name="m"></param>
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            //按快捷键     
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32())
+                    {
+                        case 81:    //按下的是CTRL+F     
+                            textBox4.Text = textBox6.Text.Trim();
+                            Clipboard.SetText(textBox6.Text.Trim());
+                            break;
+                        case 82:
+                            textBox7.Text = textBox5.Text.Trim();
+                            Clipboard.SetText(textBox5.Text.Trim());
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        /// <summary>
+        /// 获取鼠标焦点颜色
+        /// </summary>
+        /// <returns></returns>
+        private Color GetPointColor(Point screenPoint)
+        {
+            IntPtr displayDC = gdi32.CreateDC("DISPLAY", null, null, IntPtr.Zero);
+            uint colorref = gdi32.GetPixel(displayDC, screenPoint.X, screenPoint.Y);
+            gdi32.DeleteDC(displayDC);
+            byte Red = GetRValue(colorref);
+            byte Green = GetGValue(colorref);
+            byte Blue = GetBValue(colorref);
+            return Color.FromArgb(Red, Green, Blue);
+        }
+
+        /// <summary>
+        /// 不断获取鼠标所在位置的颜色
+        /// </summary>
+        private void PickeMousePointColor()
+        {
+            ControlHelper.ControlInvoke(this, new Action<String>((s) =>
+            {
+                textBox1.Text = String.Format("{0},{1}", Control.MousePosition.X.ToString(), Control.MousePosition.Y.ToString());
+                Point pt = new Point(Control.MousePosition.X, Control.MousePosition.Y);
+                Color cl = GetPointColor(pt);
+                panel1.BackColor = cl;
+                textBox5.Text = cl.R + "," + cl.G + "," + cl.B;
+                textBox6.Text = ColorTranslator.ToHtml(cl).ToString();
+                user32.RegisterHotKey(Handle, 81, KeyModifiers.Ctrl, Keys.F);
+                user32.RegisterHotKey(Handle, 82, KeyModifiers.Ctrl, Keys.G);
+                if (timer != null) timer.Change(100, Timeout.Infinite);
+            }), "");
+        }
+
+        /// <summary>
+        /// 退出按钮单击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// 窗体关闭前触发的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show(this,"确定退出程序吗？", "退出提醒", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+                e.Cancel = true;
+        }
+
+        /// <summary>
+        /// 窗体加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timer = new System.Threading.Timer(new TimerCallback((o) => { PickeMousePointColor(); }), null, 100, Timeout.Infinite);
+        }
+
+        #region "静态方法"
+
+        static public byte GetRValue(uint color)
+        {
+            return (byte)color;
+        }
+        static public byte GetGValue(uint color)
+        {
+            return ((byte)(((short)(color)) >> 8));
+        }
+        static public byte GetBValue(uint color)
+        {
+            return ((byte)((color) >> 16));
+        }
+        static public byte GetAValue(uint color)
+        {
+            return ((byte)((color) >> 24));
+        }
+
+        #endregion
+    }
+}
--- src/EdmxDocumentationGenerator/Generator.Console/Program.cs
+++ src/EdmxDocumentationGenerator/Generator.Console/Program.cs
@@ -11,22 +11,22 @@ namespace Generator.ConsoleTool
     {
         static void Main(string[] args)
         {
-            //GeneratorTool tool = new GeneratorTool();
+            GeneratorTool tool = new GeneratorTool();
             //Boolean result = tool.TryGenerate(@"D:\01.Work\Test\Test2017\trunk\TestEF\Model1.edmx",
             //                                  @"data source=(localdb)\ProjectsV13;initial catalog=Test;integrated security=True",
             //                                  "SqlServer2016.Generator.dll",
             //                                  "",
             //                                  @"D:\01.Work\Test\Test2017\trunk\TestEF\Model1.edmx");
 
-            GeneratorTool tool = new GeneratorTool();
-            var generator = tool.GetGenerateor(@"D:\01.Work\Test\Test2017\trunk\GenerateEntity\PQ_Entity\PQ.edmx",
-                                                @"DATA SOURCE=172.26.136.162:1521/KFMQCS;PASSWORD=MQCSBUS;PERSIST SECURITY INFO=True;USER ID=MQCSBUS",
-                                                "Oracle.Generator.dll",
-                                                "",
-                                                @"D:\01.Work\Test\Test2017\trunk\GenerateEntity\PQ_Entity\PQ.edmx") as Oracle.Generator.OracleGenerator;
-            var doc = generator.GetColumnDocumentation("T_EAP_CALENDAR_SETTING", "USER_CONF");
-            var data = doc.Split(new String[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
-            Console.WriteLine(doc);
+            //GeneratorTool tool = new GeneratorTool();
+            //var generator = tool.GetGenerateor(@"D:\01.Work\Test\Test2017\trunk\GenerateEntity\PQ_Entity\PQ.edmx",
+            //                                    @"DATA SOURCE=172.26.136.162:1521/KFMQCS;PASSWORD=MQCSBUS;PERSIST SECURITY INFO=True;USER ID=MQCSBUS",
+            //                                    "Oracle.Generator.dll",
+            //                                    "",
+            //                                    @"D:\01.Work\Test\Test2017\trunk\GenerateEntity\PQ_Entity\PQ.edmx") as Oracle.Generator.OracleGenerator;
+            //var doc = generator.GetColumnDocumentation("T_EAP_CALENDAR_SETTING", "USER_CONF");
+            //var data = doc.Split(new String[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            //Console.WriteLine(doc);
 
 
             //Boolean result = tool.TryGenerate(@"D:\01.Work\Test\Test2017\trunk\GenerateEntity\PQ_Entity\PQ.edmx",
@@ -37,18 +37,18 @@ namespace Generator.ConsoleTool
 
 
 
-            //Boolean result = tool.TryGenerate(@"D:\01.Work\Test\Test2017\trunk\GenerateEntity\MQCSBUS_Entity\MQCSBUS.edmx",
-            //                                  @"DATA SOURCE=172.26.136.162:1521/KFMQCS;PASSWORD=MQCSBUS;PERSIST SECURITY INFO=True;USER ID=MQCSBUS",
-            //                                  "Oracle.Generator.dll",
-            //                                  "",
-            //                                  @"D:\01.Work\Test\Test2017\trunk\GenerateEntity\MQCSBUS_Entity\MQCSBUS.edmx");
+            Boolean result = tool.TryGenerate(@"D:\01.Work\szlanyou\TQMP_公共产出\00_源程序\LY.CommonCodes\LY.CommonCodes.GenerateEntity\PTMC\DB_PTMC.edmx",
+                                              @"DATA SOURCE=172.26.175.94:1521/PTMCDB;PASSWORD=PTMC;PERSIST SECURITY INFO=True;USER ID=PTMC",
+                                              "Oracle.Generator.dll",
+                                              "",
+                                              @"D:\01.Work\szlanyou\TQMP_公共产出\00_源程序\LY.CommonCodes\LY.CommonCodes.GenerateEntity\PTMC\DB_PTMC.edmx");
 
             //Boolean result = tool.TryGenerate(@"D:\01.Work\Test\Test2017\trunk\GenerateEntity\DB_Entity\DB.edmx",
             //                                  @"DATA SOURCE=172.26.136.162:1521/KFMQCS;PASSWORD=DB;PERSIST SECURITY INFO=True;USER ID=DB",
             //                                  "Oracle.Generator.dll",
             //                                  "",
             //                                  @"D:\01.Work\Test\Test2017\trunk\GenerateEntity\DB_Entity\DB.edmx");
-            //if (!result)
+            if (!result)
                 Console.Read();
         }
     }
